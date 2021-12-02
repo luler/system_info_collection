22b3f7f8ee2c0ff42709759f0725e42021b72a2f
1207032539@qq.com <1207032539@qq.com>
2021-12-02 10:21:16 +0800

    安装程序优化4

--- shell/install.sh
+++ shell/install.sh
@@ -4,7 +4,7 @@ BASE_PAHT=$(cd $(dirname $0) && cd ../../ && pwd)
 
 echo '正在检查并安装python的依赖包...'
 PYTHON_VERSION=$(python -V 2>&1 | awk '{print $2}' | awk -F '.' '{print $1}')
-yum install -y pyhon-pip
+yum install -y python-pip
 if [ $PYTHON_VERSION = 2 ]; then
   pip install -i https://mirrors.aliyun.com/pypi/simple/ setuptools==33.1.1
 fi
