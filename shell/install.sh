958a8cd37ebe2a6a486ad548df19e065f994107d
1207032539@qq.com <1207032539@qq.com>
2021-12-02 09:51:32 +0800

    安装程序优化3

--- shell/install.sh
+++ shell/install.sh
@@ -6,9 +6,9 @@ echo '正在检查并安装python的依赖包...'
 PYTHON_VERSION=$(python -V 2>&1 | awk '{print $2}' | awk -F '.' '{print $1}')
 yum install -y pyhon-pip
 if [ $PYTHON_VERSION = 2 ]; then
-  pip install setuptools==33.1.1
+  pip install -i https://mirrors.aliyun.com/pypi/simple/ setuptools==33.1.1
 fi
-pip install -r $BASE_PAHT/system_info_collection/requirements.txt
+pip install -i https://mirrors.aliyun.com/pypi/simple/ -r $BASE_PAHT/system_info_collection/requirements.txt
 echo '检查并安装python的依赖包成功'
 
 echo '正在复制代码程序代码...'
