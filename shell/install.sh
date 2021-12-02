757dcf2b051d26a3b824ee6ad87cbb00b7d368b4
1207032539@qq.com <1207032539@qq.com>
2021-12-02 09:02:11 +0800

    安装程序优化

--- shell/install.sh
+++ shell/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 BASE_PAHT=$(cd $(dirname $0) && cd ../../ && pwd)
+
+echo '正在检查并安装python的依赖包...'
+PYTHON_VERSION=$(python -V 2>&1 | awk '{print $2}' | awk -F '.' '{print $1}')
+if [ $PYTHON_VERSION = 2 ]; then
+  pip install setuptools==33.1.1
+fi
+pip install -r $BASE_PAHT/system_info_collection/requirements.txt
+echo '检查并安装python的依赖包成功'
+
 echo '正在复制代码程序代码...'
 cp -r $BASE_PAHT/system_info_collection /usr/local
 echo '复制代码程序代码完成'
