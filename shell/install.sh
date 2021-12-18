f6ff98eb2ec492cd364ca2f58edcefb26a0d5306
我只想看看蓝天 <1207032539@qq.com>
2021-12-18 21:18:44 +0800

    优化

--- shell/install.sh
+++ shell/install.sh
@@ -5,27 +5,67 @@ set -e
 
 BASE_PAHT=$(cd $(dirname $0) && cd ../../ && pwd)
 
-echo '正在检查并安装python的依赖包...'
-if [ "`command -v python3`" = "" ] || [ "`command -v pip3`" = "" ]; then
-    yum install -y epel-release gcc python3 python3-devel
+type=''
+if [ -f /etc/redhat-release ]i; then
+        type='centos'
 fi
-pip3 install -i https://mirrors.aliyun.com/pypi/simple/ -r $BASE_PAHT/system_info_collection/requirements.txt
-echo '检查并安装python的依赖包成功'
-
-echo '正在复制代码程序代码...'
-cp -r $BASE_PAHT/system_info_collection /usr/local
-echo '复制代码程序代码完成'
-
-echo '正在初始化系统服务...'
-cp $BASE_PAHT/system_info_collection/shell/centos/system_info_collection.sh /etc/init.d/system_info_collection
-chmod +x /etc/init.d/system_info_collection
-chkconfig --add system_info_collection
-chkconfig system_info_collection on
-echo '初始化系统服务完成'
-
-echo '正在启动系统服务...'
-if [ "`command -v service`" = "" ]; then
-    yum install -y epel-release initscripts
+if [ `cat /etc/issue | grep -i -c ubuntu` = 1 ]; then
+        type='ubuntu'
 fi
-service system_info_collection start
-echo '启动系统服务完成'
+
+case $type in
+
+        'ubuntu')
+        echo '正在检查并安装python的依赖包...'
+        if [ "`command -v python3`" = "" ] || [ "`command -v pip3`" = "" ]; then
+            apt install -y epel-release gcc python3 python3-pip
+        fi
+        pip3 install -i https://mirrors.aliyun.com/pypi/simple/ -r $BASE_PAHT/system_info_collection/requirements.txt
+        echo '检查并安装python的依赖包成功'
+
+        echo '正在复制代码程序代码...'
+        cp -r $BASE_PAHT/system_info_collection /usr/local
+        echo '复制代码程序代码完成'
+
+        echo '正在初始化系统服务...'
+        cp $BASE_PAHT/system_info_collection/shell/init.d/system_info_collection.sh /etc/init.d/system_info_collection
+        chmod +x /etc/init.d/system_info_collection
+        update-rc.d system_info_collection defaults
+        echo '初始化系统服务完成'
+
+        echo '正在启动系统服务...'
+        service system_info_collection start
+        echo '启动系统服务完成'
+        ;;
+
+        'centos')
+        echo '正在检查并安装python的依赖包...'
+        if [ "`command -v python3`" = "" ] || [ "`command -v pip3`" = "" ]; then
+            yum install -y epel-release gcc python3 python3-devel
+        fi
+        pip3 install -i https://mirrors.aliyun.com/pypi/simple/ -r $BASE_PAHT/system_info_collection/requirements.txt
+        echo '检查并安装python的依赖包成功'
+
+        echo '正在复制代码程序代码...'
+        cp -r $BASE_PAHT/system_info_collection /usr/local
+        echo '复制代码程序代码完成'
+
+        echo '正在初始化系统服务...'
+        cp $BASE_PAHT/system_info_collection/shell/init.d/system_info_collection.sh /etc/init.d/system_info_collection
+        chmod +x /etc/init.d/system_info_collection
+        chkconfig --add system_info_collection
+        chkconfig system_info_collection on
+        echo '初始化系统服务完成'
+
+        echo '正在启动系统服务...'
+        if [ "`command -v service`" = "" ]; then
+            yum install -y epel-release initscripts
+        fi
+        service system_info_collection start
+        echo '启动系统服务完成'
+        ;;
+
+        *)
+        echo '当前系统非centos和ubuntu系统，不支持一键安装，请自行手动安装!'
+        ;;
+esac
--- shell/uninstall.sh
+++ shell/uninstall.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+set -x
+
+type=''
+if [ -f /etc/redhat-release ]i; then
+        type='centos'
+fi
+if [ `cat /etc/issue | grep -i -c ubuntu` = 1 ]; then
+        type='ubuntu'
+fi
+
 echo '正在关闭相关系统服务...'
 service system_info_collection stop
 echo '关闭相关系统服务完成'
 
-echo '正在删除系统服务...'
-chkconfig system_info_collection off
-chkconfig --del system_info_collection
-rm -f /etc/init.d/system_info_collection
-echo '删除系统服务完成'
+case $type in
+
+        'ubuntu')
+        echo '正在删除系统服务...'
+        update-rc.d -f system_info_collection remove
+        rm -f /etc/init.d/system_info_collection
+        echo '删除系统服务完成'
+        ;;
+
+        'centos')
+        echo '正在删除系统服务...'
+        chkconfig system_info_collection off
+        chkconfig --del system_info_collection
+        rm -f /etc/init.d/system_info_collection
+        echo '删除系统服务完成'
+        ;;
+
+        *)
+        echo '当前系统非centos和ubuntu系统，不支持一键卸载，请自行手动卸载!'
+        ;;
+esac
 
 echo '正在清理代码程序代码...'
 rm -rf /usr/local/system_info_collection
